da3e91157c13a57b22cb441006373ed31c2d4b72
NFMdev <88680241+NFMdev@users.noreply.github.com>
2026-02-02 18:05:18 +0100

    Rename project references

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sembo-cv-project/",
+  base: "/cv-web-app/",
 })
